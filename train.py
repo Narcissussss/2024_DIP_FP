c8d12ea56967c4b03eb3c441641a3e7c01a25535
Narcissussss <153915485+Narcissussss@users.noreply.github.com>
2024-12-30 10:52:20 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 from ultralytics import YOLO
 model = YOLO("yolo11n-seg.pt")  # build a new model from YAML
-model.train(data='/home/icssl_pub/Project/Narcissus/DIP_FP/YOLO_v11/config.yaml',epochs=50,imgsz=640)
+model.train(data='/home/icssl_pub/Project/Narcissus/DIP_FP/YOLO_v11/config.yaml',epochs=50,imgsz=640,pretrained=False)
